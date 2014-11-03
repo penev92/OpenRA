7a88f816c522e8f75b18666bc03e64cfe4e14139
penev92 <penev92@gmail.com>
2014-11-03 13:01:10 +0200

    Code style fixes
    Fixed AttackWander, addressed style nits
    
    Fix Travis crash

--- OpenRA.Mods.D2k/Sandworm.cs
+++ OpenRA.Mods.D2k/Sandworm.cs
@@ -17,43 +17,15 @@ namespace OpenRA.Mods.D2k
 {
 	class SandwormInfo : Requires<RenderUnitInfo>, Requires<MobileInfo>, IOccupySpaceInfo
 	{
-		readonly public int WanderMoveRadius = 20;
 		readonly public string WormSignNotification = "WormSign";
 		
 		public object Create(ActorInitializer init) { return new Sandworm(this); }
 	}
 
-	class Sandworm : INotifyIdle
+	class Sandworm
 	{
-		int ticksIdle;
-		int effectiveMoveRadius;
-		readonly int maxMoveRadius;
-
 		public Sandworm(SandwormInfo info)
 		{
-			maxMoveRadius = info.WanderMoveRadius;
-			effectiveMoveRadius = info.WanderMoveRadius;
-		}
-
-		// TODO: This copies AttackWander and builds on top of it. AttackWander should be revised.
-		public void TickIdle(Actor self)
-		{
-			var target = self.CenterPosition + new WVec(0, -1024 * effectiveMoveRadius, 0).Rotate(WRot.FromFacing(self.World.SharedRandom.Next(255)));
-			var targetCell = self.World.Map.CellContaining(target);
-
-			if (!self.World.Map.Contains(targetCell))
-			{
-				// If MoveRadius is too big there might not be a valid cell to order the attack to (if actor is on a small island and can't leave)
-				if (++ticksIdle % 10 == 0)      // completely random number
-					effectiveMoveRadius--;
-
-				return;  // We'll be back the next tick; better to sit idle for a few seconds than prolongue this tick indefinitely with a loop
-			}
-
-			self.World.IssueOrder(new Order("AttackMove", self, false) { TargetLocation = targetCell });
-
-			ticksIdle = 0;
-			effectiveMoveRadius = maxMoveRadius;
 		}
 	}
 }

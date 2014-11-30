62649e91114ed694935f7d75f8debc3b85c6719e
penev92 <penev92@gmail.com>
2014-11-30 15:39:47 +0200

    Add RadarPing on worm spawn, fix wandering issue, add new Wormspawner.shp

--- OpenRA.Mods.RA/AttackMove.cs
+++ OpenRA.Mods.RA/AttackMove.cs
@@ -48,7 +48,8 @@ namespace OpenRA.Mods.RA
 
 		public void TickIdle(Actor self)
 		{
-			if (TargetLocation.HasValue)
+			// This might cause the actor to be stuck if the target location is unreachable
+			if (TargetLocation.HasValue && self.Location != TargetLocation.Value)
 				Activate(self);
 		}
 

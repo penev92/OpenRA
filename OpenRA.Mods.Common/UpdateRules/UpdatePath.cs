31b0b46fba8f3cf9e43f9bab224bbfff6e0daff5
Pavel Penev <penev92@gmail.com>
2020-04-27 01:43:57 +0300

    Add configurable squadrons to AirstrikePower

--- OpenRA.Mods.Common/UpdateRules/UpdatePath.cs
+++ OpenRA.Mods.Common/UpdateRules/UpdatePath.cs
@@ -85,6 +85,7 @@ namespace OpenRA.Mods.Common.UpdateRules
 				new ExtractResourceStorageFromHarvester(),
 				new ReplacePaletteModifiers(),
 				new RemoveConyardChronoReturnAnimation(),
+				new ImproveAirstrikePowerConfiguration(),
 
 				// Execute these rules last to avoid premature yaml merge crashes.
 				new ReplaceCloakPalette(),

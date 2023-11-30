7383ba0bda0cf2fcce9418b1ace8fba7ef0945ae
penev92 <penev92@gmail.com>
2023-11-30 23:07:36 +0200

    f

--- OpenRA.SourceGenerator/HelloWorldGenerator.cs
+++ OpenRA.SourceGenerator/HelloWorldGenerator.cs
@@ -21,7 +21,6 @@ namespace OpenRA.SourceGenerators
 
 		public void Execute(GeneratorExecutionContext context)
 		{
-
 			foreach (var classDeclaration in ((HelloWorldSyntaxContextReceiver)context.SyntaxContextReceiver).ClassDeclarations)
 			{
 				if (classDeclaration.Parent is not NamespaceDeclarationSyntax namespaceDeclaration)
@@ -30,25 +29,8 @@ namespace OpenRA.SourceGenerators
 				var semanticModel = context.Compilation.GetSemanticModel(classDeclaration.SyntaxTree);
 				var typeSymbol = semanticModel.GetDeclaredSymbol(classDeclaration) as ITypeSymbol;
 				var syncedClassMembers = GetAllMembers(typeSymbol, x => HasSyncAttribute(x)).ToArray();
-				var hashCodeStrings = new List<string>();
-				var syncsTargets = false;
-				//foreach (var member in syncedClassMembers)
-				//{
-				//	var isTarget = false;
-				//	if (member is IFieldSymbol field)
-				//		hashCodeStrings.Add(SyncHelpers.GetHashCodeString(field.Type.Name, member.Name, out isTarget));
-
-				//	if (member is IPropertySymbol property)
-				//		hashCodeStrings.Add(SyncHelpers.GetHashCodeString(property.Type.Name, member.Name, out isTarget));
-
-				//	syncsTargets |= isTarget;
-				//}
-
-				//var isSealed = classDeclaration.IsSealed();
-				//var sourceCode = GenerateClassCode(namespaceDeclaration.Name, classDeclaration.Identifier.Text, hashCodeStrings, isSealed, syncsTargets);
-				//context.AddSource($"{classDeclaration.Identifier.Text}.g.cs", sourceCode);
+				Console.WriteLine(syncedClassMembers);
 			}
-
 
 			// begin creating the source we'll inject into the users compilation
 			var sourceCode = SourceText.From(@"

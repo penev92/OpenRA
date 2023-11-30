500d6b00e414f642f80e60466375bb6ec92db605
penev92 <penev92@gmail.com>
2023-11-30 23:19:18 +0200

    Make it look real

--- OpenRA.SourceGenerator/HelloWorldGenerator.cs
+++ OpenRA.SourceGenerator/HelloWorldGenerator.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Text;
 using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Text;
-using OpenRA.SourceGenerator;
 
 namespace OpenRA.SourceGenerators
 {
@@ -16,22 +11,10 @@ namespace OpenRA.SourceGenerators
 
 		public void Initialize(GeneratorInitializationContext context)
 		{
-			context.RegisterForSyntaxNotifications(() => new HelloWorldSyntaxContextReceiver());
 		}
 
 		public void Execute(GeneratorExecutionContext context)
 		{
-			foreach (var classDeclaration in ((HelloWorldSyntaxContextReceiver)context.SyntaxContextReceiver).ClassDeclarations)
-			{
-				if (classDeclaration.Parent is not NamespaceDeclarationSyntax namespaceDeclaration)
-					return;
-
-				var semanticModel = context.Compilation.GetSemanticModel(classDeclaration.SyntaxTree);
-				var typeSymbol = semanticModel.GetDeclaredSymbol(classDeclaration) as ITypeSymbol;
-				var syncedClassMembers = GetAllMembers(typeSymbol, x => HasSyncAttribute(x)).ToArray();
-				Console.WriteLine(syncedClassMembers);
-			}
-
 			// begin creating the source we'll inject into the users compilation
 			var sourceCode = SourceText.From(@"
 #pragma warning disable 1591
@@ -44,20 +27,6 @@ public static class GeneratedCode
 ", Encoding.UTF8);
 
 			context.AddSource("GeneratedCode.g.cs", sourceCode);
-		}
-
-		public static bool HasSyncAttribute(ISymbol symbol) => symbol.GetAttributes().Any(x => x.AttributeClass.Name == SyncAttributeName);
-
-		public static IEnumerable<ISymbol> GetAllMembers(ITypeSymbol typeSymbol, Func<ISymbol, bool> predicate)
-		{
-			foreach (var symbol in typeSymbol.GetMembers().Where(predicate))
-				yield return symbol;
-
-			if (typeSymbol.BaseType == null)
-				yield break;
-
-			foreach (var symbol in GetAllMembers(typeSymbol.BaseType, predicate))
-				yield return symbol;
 		}
 	}
 }
---
+++ OpenRA.SourceGenerator/Sync/SyncHelpers.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+
+namespace OpenRA.SourceGenerators.Sync
+{
+	static class SyncHelpers
+	{
+		public const string SyncAttributeName = "SyncAttribute";
+		public const string SyncInterfaceName = "OpenRA.ISync";
+
+		public static string GetHashCodeString(string typeName, string objectName, out bool isTarget)
+		{
+			isTarget = typeName == "Target";
+			return typeName switch
+			{
+				"Boolean" => $"({objectName} ? 1 : 0)",
+				"Int32" => objectName,
+				"int2" => $"((({objectName}.X * 5) ^ ({objectName}.Y * 3)) / 4)",
+				"CPos" => $"{objectName}.Bits",
+				"CVec" => $"((({objectName}.X * 5) ^ ({objectName}.Y * 3)) / 4)",
+				"WDist" => $"{objectName}.Length",
+				"WPos" => $"{objectName}.X ^ {objectName}.Y ^ {objectName}.Z",
+				"WVec" => $"{objectName}.X ^ {objectName}.Y ^ {objectName}.Z",
+				"WAngle" => $"{objectName}.Angle",
+				"WRot" => $"{objectName}.Roll ^ {objectName}.Pitch ^ {objectName}.Yaw",
+				"Actor" => $"({objectName} == null ? 0 : (int)({objectName}.ActorID << 16))",
+				"Player" => $"({objectName} == null ? 0 : (int)({objectName}.PlayerActor.ActorID << 16) * 0x567)",
+				"Target" => $"HashTarget({objectName})",
+				_ => throw new NotSupportedException()
+			};
+		}
+
+		public static bool ImplementsISync(this INamedTypeSymbol type) => type.AllInterfaces.Any(y => y.ToDisplayString() == SyncInterfaceName);
+
+		public static bool HasSyncAttribute(ISymbol symbol) => symbol.GetAttributes().Any(x => x.AttributeClass.Name == SyncAttributeName);
+
+		public static string GetTargetSyncHashMethod() => $@"
+		static int HashTarget(Target t)
+		{{
+			switch (t.Type)
+			{{
+				case TargetType.Actor:
+					return {GetHashCodeString("Actor", "t.Actor", out _)};
+
+				case TargetType.FrozenActor:
+					var actor = t.FrozenActor.Actor;
+					if (actor == null)
+						return 0;
+
+					return {GetHashCodeString("Actor", "actor", out _)};
+
+				case TargetType.Terrain:
+					return {GetHashCodeString("WPos", "t.CenterPosition", out _)};
+
+				case TargetType.Invalid:
+				default:
+					return 0;
+			}}
+		}}";
+	}
+}

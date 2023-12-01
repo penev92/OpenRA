f0609d13ed46cabd5f954acfc04503cb17453043
penev92 <penev92@gmail.com>
2023-12-01 11:28:49 +0200

    Revert "Will the v2 (incremental) source generator work #2"
    This reverts commit e173cf121adaf45fec193fb92b44a96c7c726381.

--- OpenRA.SourceGenerator/SourceGeneratorHelpers.cs
+++ OpenRA.SourceGenerator/SourceGeneratorHelpers.cs
@@ -1,15 +1,4 @@
-﻿#region Copyright & License Information
-/*
- * Copyright (c) The OpenRA Developers and Contributors
- * This file is part of OpenRA, which is free software. It is made
- * available to you under the terms of the GNU General Public License
- * as published by the Free Software Foundation, either version 3 of
- * the License, or (at your option) any later version. For more
- * information, see COPYING.
- */
-#endregion
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.CodeAnalysis;
@@ -19,9 +8,8 @@ namespace OpenRA.SourceGenerators
 {
 	static class SourceGeneratorHelpers
 	{
-		public static bool IsPartialClass(this SyntaxNode node)
-			=> node is ClassDeclarationSyntax classDeclaration
-				&& classDeclaration.Modifiers.Any(x => x.IsKind(Microsoft.CodeAnalysis.CSharp.SyntaxKind.PartialKeyword));
+		public static bool IsPartial(this ClassDeclarationSyntax classDeclaration)
+			=> classDeclaration.Modifiers.Any(x => x.IsKind(Microsoft.CodeAnalysis.CSharp.SyntaxKind.PartialKeyword));
 
 		public static bool IsSealed(this ClassDeclarationSyntax classDeclaration)
 			=> classDeclaration.Modifiers.Any(x => x.IsKind(Microsoft.CodeAnalysis.CSharp.SyntaxKind.SealedKeyword));

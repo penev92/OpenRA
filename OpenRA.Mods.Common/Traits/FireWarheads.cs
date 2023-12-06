b6a958ddd6de24d45428ed168e931653083c6e31
Pavel Penev <penev92@gmail.com>
2023-12-06 23:33:33 +0200

    Switched implementing ISync to GenerateSyncCode
    Types that have members with `SyncMemberAttribute`  MUST be partial AND MUST either have or inherit `GenerateSyncCodeAttribute`.

--- OpenRA.Mods.Common/Traits/FireWarheads.cs
+++ OpenRA.Mods.Common/Traits/FireWarheads.cs
@@ -48,7 +48,8 @@ namespace OpenRA.Mods.Common.Traits
 		}
 	}
 
-	public class FireWarheads : PausableConditionalTrait<FireWarheadsInfo>, ITick, ISync
+	[GenerateSyncCode]
+	public partial class FireWarheads : PausableConditionalTrait<FireWarheadsInfo>, ITick
 	{
 		[Sync]
 		int cooldown = 0;
